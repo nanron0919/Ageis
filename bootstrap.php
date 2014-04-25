77ce8136e585092980d03d50ddc7907588a330d8
Norman <Norman@NormanMacBook-Air-3.local>
2014-04-25 21:09:03 +0800

    exec autoload to load necessary module

--- bootstrap.php
+++ bootstrap.php
@@ -2,15 +2,6 @@
 /**
  * bootstrap
  */
-$included = get_included_files();
-define('FRAMEWORK_ROOT', dirname(__FILE__)); // without trailing slash
-define('APP_ROOT', dirname(dirname($included[0]))); // without trailing slash
-
-// autoload application
-include_once(FRAMEWORK_ROOT . '/_core/class.application.php');
-
-header("Content-Type:text/html; charset=utf-8");
-$application = new Application;
+$application = require __DIR__ . '/autoload.php';
 $application->run();
-
 ?>
