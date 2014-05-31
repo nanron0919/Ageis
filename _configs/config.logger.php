de495199716e984d04438c77d9d36756bd00f5a6
Norman_Chen <nanron0919@gmail.com>
2014-05-31 10:42:56 +0800

    implement logger

---
+++ _configs/config.logger.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * config logger
+ */
+return array(
+    'root' => '_log',
+    'levels' => array(
+        'notice' => array(),
+        'warning' => array(),
+        'fatal' => array(),
+    ),
+);
+?>
