8ba21c14980f2d99d36e06a7fe646fd128aa01de
Norman <Norman@NormanMacBook-Air-4.local>
2014-05-31 10:05:47 +0800

    clean setting config

--- _configs/config.session.php
+++ _configs/config.session.php
@@ -8,10 +8,7 @@ return array(
     // essential settings below, do not remove //
     /////////////////////////////////////////////
 
-    'lifetime' => 120, // second
     'files' => APP_ROOT . '/_sessions',
-    'path' => '/',
-    'secure' => false,
 );
 
 ?>
