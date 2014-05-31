26033a7598647ea557ae012c5befca70c094e9c1
Norman_Chen <nanron0919@gmail.com>
2014-05-31 10:47:35 +0800

    add home language file

---
+++ _i18n/zh-tw/lang.home.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * setting for home
+ */
+return array(
+    'title' => 'Ageis\'s Home',
+    'framework_name' => 'Ageis',
+    'lorem' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, neque dolorum expedita at magnam dicta debitis aut deleniti eveniet maxime qui officiis natus ad modi provident repellat ea hic recusandae.',
+    'github' => 'Check out source at Github',
+    'copyright' => 'Copyright By',
+    'author_email' => 'nanron0919@gmail.com',
+    'author' => 'Norman Chen'
+);
+?>
