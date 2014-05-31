49aac13c62862818afc3a1dda65dc96d5d28641e
Norman_Chen <nanron0919@gmail.com>
2014-05-31 10:33:26 +0800

    implemnt file cache

---
+++ _configs/config.caching.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * config caching
+ */
+return array(
+    /////////////////////////////////////////////
+    // essential settings below, do not remove //
+    /////////////////////////////////////////////
+    'expired' => 3660,   // 1 hour, 0 means forever
+    'path' => '_tmp/caching'
+);
+?>
