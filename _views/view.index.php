79f102e547d29d647e8049d42d17e40a59210c1a
Norman_Chen <nanron0919@gmail.com>
2014-05-31 10:53:02 +0800

    add view to present

---
+++ _views/view.index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $lang['title']; ?></title>
+    <link rel="stylesheet" href="assets/css/test.css">
+</head>
+<body>
+    <header>
+        <h1><?php echo $lang['framework_name']; ?></h1>
+        <a href="https://github.com/nanron0919/Ageis" target="_blank"><?php echo $lang['github']; ?></a>
+    </header>
+    <div class="wrapper section">
+        <article class="hero">
+            <h1>A lightweight, easy grasp framework</h1>
+        </article>
+        <p><?php echo $lang['lorem']; ?></p>
+    </div>
+    <footer>
+        <?php echo $lang['copyright']; ?>
+        <a href="mailto:<?php echo $lang['author_email']; ?>"><?php echo $lang['author']; ?></a>
+    </footer>
+</body>
+</html>
