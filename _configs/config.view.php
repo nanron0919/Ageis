8efcdbc4270928c07a38c886bb90e07e47c248e5
Norman <Norman@NormanMacBook-Air-4.local>
2014-05-31 10:06:20 +0800

    add more settings

--- _configs/config.view.php
+++ _configs/config.view.php
@@ -4,6 +4,9 @@ return array(
     // essential settings below, do not remove //
     /////////////////////////////////////////////
 
-    'path' => APP_ROOT . '/_views'
+    'path' => APP_ROOT . '/_views',
+    'cache' => true,
+    'caching_path' =>  '_tmp/views',
+    'expired' => 3600,
 );
 ?>
