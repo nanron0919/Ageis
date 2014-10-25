bd9ec4e33ea4163b588a3d70ca1f53c46d55eafb
Norman_Chen <nanron0919@gmail.com>
2014-10-25 19:39:27 +0800

    bug fixed

--- _views/view.index.php
+++ _views/view.index.php
@@ -2,23 +2,21 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $lang['title']; ?></title>
+    <title>Home</title>
     <link rel="stylesheet" href="assets/css/test.css">
 </head>
 <body>
     <header>
-        <h1><?php echo $lang['framework_name']; ?></h1>
-        <a href="https://github.com/nanron0919/Ageis" target="_blank"><?php echo $lang['github']; ?></a>
+        <h1>Ageis - Light PHP Framework</h1>
+        <p>Go to <a href="https://github.com/nanron0919/Ageis" target="_blank">Github</a></p>
     </header>
     <div class="wrapper section">
-        <article class="hero">
-            <h1>A lightweight, easy grasp framework</h1>
+        <article>
+            <h1>Hello world</h1>
         </article>
-        <p><?php echo $lang['lorem']; ?></p>
     </div>
     <footer>
-        <?php echo $lang['copyright']; ?>
-        <a href="mailto:<?php echo $lang['author_email']; ?>"><?php echo $lang['author']; ?></a>
+        <a href="mailto:nanron0919@gmail.com">Email me</a>
     </footer>
 </body>
 </html>
